431ae098d51f04f413a60356d5a5869204fc65c1
Nelson Ramirez <xhyskoax@gmail.com>
2024-08-28 21:31:21 -0400

    Se agregan ejemplos de abstraccion, encapsulamiento y decoradores [property]

---
+++ 4.abstraccion.py
@@ -0,0 +1,33 @@
+class CuentaBancaria:
+    def __init__(self, titular, saldo=0):
+        self._titular = titular
+        self._saldo = saldo
+    
+    def depositar(self, monto):
+        if monto > 0:
+            self._saldo += monto
+            print(f"Depósito exitoso. Nuevo saldo: {self._saldo}")
+        else:
+            print("El monto debe ser positivo.")
+
+    def retirar(self, monto):
+        if 0 < monto <= self._saldo:
+            self._saldo -= monto
+            print(f"Retiro exitoso. Nuevo saldo: {self._saldo}")
+        else:
+            print("Fondos insuficientes o monto inválido.")
+    
+    def mostrar_saldo(self):
+        print(f"Saldo actual: {self._saldo}")
+        
+cuenta_pepito = CuentaBancaria("Pepito")
+
+cuenta_pepito.mostrar_saldo()
+
+cuenta_pepito.depositar(6000)
+
+cuenta_pepito.retirar(5000)
+
+cuenta_pepito._saldo = 500000
+
+print(cuenta_pepito._saldo)
---
+++ 5.encapsulamiento.py
@@ -0,0 +1,30 @@
+class CuentaBancaria:
+    def __init__(self, titular, saldo=0):
+        self.__titular = titular
+        self.__saldo = saldo
+    
+    def depositar(self, monto):
+        if monto > 0:
+            self.__saldo += monto
+            print(f"Depósito exitoso. Nuevo saldo: {self.__saldo}")
+        else:
+            print("El monto debe ser positivo.")
+
+    def retirar(self, monto):
+        if 0 < monto <= self.__saldo:
+            self.__saldo -= monto
+            print(f"Retiro exitoso. Nuevo saldo: {self.__saldo}")
+        else:
+            print("Fondos insuficientes o monto inválido.")
+    
+    def mostrar_saldo(self):
+        print(f"Saldo actual: {self.__saldo}")
+        
+cuenta_pepito = CuentaBancaria("Pepito")
+
+cuenta_pepito.__saldo = 600000
+
+
+cuenta_pepito.depositar(5000)
+
+cuenta_pepito.mostrar_saldo()
